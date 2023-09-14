778709829d7b4f199c9d86ffecdb5beee22895ef
quirkexplorer <quirkyexplorer@gmail.com>
2023-09-14 16:20:47 -0700

    added code to make it green again

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,6 @@
 export function isItPalindrome(input: String): boolean {
     
-    const result = input.split('').reverse().join('').toLowerCase();
-   // console.log(result);
-    return result === input.toLowerCase();
+    const result = input.split('').reverse().join('').toLowerCase().replace(/\s/g, '');
+    //console.log(result);
+    return result === input.toLowerCase().replace(/\s/g, '');
 } 
