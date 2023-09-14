53172dfca77ef89497d72cafe0256f09fe9298e2
quirkexplorer <quirkyexplorer@gmail.com>
2023-09-14 16:16:14 -0700

    in red

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -17,4 +17,19 @@ describe('palindrome checker', () => {
         expect(result).toBeTruthy();
     });
 
+    it('detects string "WoWx" is not a palindrome', () => {
+        const result = isItPalindrome('WoWx');
+        expect(result).toBeFalsy();
+    });
+
+    it('detects string "xWowx" is still a palindrome', () => {
+        const result = isItPalindrome('xWowx');
+        expect(result).toBeTruthy();
+    });
+
+    it('detects string "Was It A Rat I Saw" is still a palindrome', () => {
+        const result = isItPalindrome('Was It A Rat I Saw');
+        expect(result).toBeTruthy();
+    });
+
 })
