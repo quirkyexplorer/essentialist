837378441c5db9a0b088d45f6e8f1d961e7eea6b
quirkexplorer <quirkyexplorer@gmail.com>
2023-09-23 16:34:12 -0700

    on green, all tests passed for one condition length

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -14,18 +14,31 @@ describe('password validator', () => {
   })
 
   it('tests if abcdefghijklmno is between 5 and 15 characters', () => {
+
+    const expected = {
+        isValid: true,
+        error: 'valid password'
+    }
     const result = validate('abcdefghijklmno');
-    expect(result).toBe(true);
+    expect(result).toEqual(expected);
   })
 
   it('tests if abcdefghijklmnop is between 5 and 15 characters', () => {
+    const expected = {
+        isValid: false,
+        error: 'error password must be between 5 and 15 characters'
+    }
     const result = validate('abcdefghijklmnop');
-    expect(result).toBe(false);
+    expect(result).toEqual(expected);
   })
   
   it('tests if hell is between 5 and 15 characters', () => {
+    const expected = {
+        isValid: false,
+        error: 'error password must be between 5 and 15 characters'
+    }
     const result = validate('hell');
-    expect(result).toBe(false);
+    expect(result).toEqual(expected);
   })
 
 
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -8,7 +8,8 @@ export default function validate(input: string) {
     }
 
     if (input.length < 5 || input.length > 15) {
-    
+        message.isValid = false
+        message.error = 'error password must be between 5 and 15 characters'
     }
 
     return message;
