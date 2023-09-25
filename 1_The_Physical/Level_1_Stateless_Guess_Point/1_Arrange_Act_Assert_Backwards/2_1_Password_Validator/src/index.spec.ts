1c1722094b33d71466f8d5584b0256a3a29b1a0f
quirkexplorer <quirkyexplorer@gmail.com>
2023-09-24 19:44:08 -0700

    refactored the previous green code, parameritized the tests

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -11,192 +11,119 @@ describe('password validator', () => {
     // digit
     // uppercase 
 
-    // 4 character word = cats
-    // > 15 character word = electroencephalograph
-    // 14 character word + number =
-    // 4 character word + number = 
-    // 15 character word = acclimatization
+    const testCases: Array<[string, { isValid: boolean, error: Record<string, string> }]>  = [
+        ['cat', {
+            isValid: false,
+            error: {
+                    Length: 'password must be between 5 and 15 characters',
+                    Digit: 'password must have at least one digit',
+                    Case: 'password must have at least one Uppercase'
+            }
+        }],
 
-
- // tests fails for length digit and upppercase
-  it('tests if cat is between 5 and 15 characters', () => {
-
-    const expected = {
-        isValid: false,
-        error: {
+        [ 'electroencephalograph', {
+            isValid: false,
+            error: {
+                    Length: 'password must be between 5 and 15 characters',
+                    Digit: 'password must have at least one digit',
+                    Case: 'password must have at least one Uppercase'
+            }
+        }],
+        [ 'Cat',  {
+            isValid: false,
+            error: {
                 Length: 'password must be between 5 and 15 characters',
                 Digit: 'password must have at least one digit',
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-    const result = validate('cat');
-    console.log(result);
-    expect(result).toEqual(expected);
-  })
+            }
+        }],
 
-  it('tests if electroencephalograph is between 5 and 15 characters', () => {
-
-    const expected = {
-        isValid: false,
-        error: {
-                Length: 'password must be between 5 and 15 characters',
-                Digit: 'password must have at least one digit',
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-    const result = validate('electroencephalograph');
-    expect(result).toEqual(expected);
-  })
-
-  // tests fails for length and digit only - length has 2 types over the limit and under limit
-
-  it('tests if Cat is between 5 and 15 characters', () => {
-    const expected = {
-        isValid: false,
-        error: {
+        [ 'Electroencephalograph', {
+            isValid: false,
+            error: {
             Length: 'password must be between 5 and 15 characters',
             Digit: 'password must have at least one digit',
         }
-    }
-    const result = validate('Cat');
-    expect(result).toEqual(expected);
-  })
-  
-  it('tests if Electroencephalograph is between 5 and 15 characters', () => {
-    const expected = {
-        isValid: false,
-        error: {
+        }],
+
+        ['cat1', {
+            isValid: false,
+            error: {
             Length: 'password must be between 5 and 15 characters',
-            Digit: 'password must have at least one digit',
-        }
-    }
-    const result = validate('Electroencephalograph');
-    expect(result).toEqual(expected);
-  })
-
-// test fails for length and uppercase only  - length has upper and lower limit words
-
-  it('tests if cat1 is at least one digit in the password', () => {
-    const expected = {
-        isValid: false,
-        error: {
-                Length: 'password must be between 5 and 15 characters',
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-
-    const result = validate('cat1');
-    expect(result).toEqual(expected);
-  })
-
-  it('tests if electroencephalograph3 is between 5 and 15 characters', () => {
-    const expected = {
-        isValid: false,
-        error: {
-                Length: 'password must be between 5 and 15 characters',
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-
-    const result = validate('electroencephalograph3');
-    expect(result).toEqual(expected);
-  })
-
-// test fails for digit and uppercase
-
-it('tests if ocean is invalid and matches errors', () =>{
-    const expected = {
-        isValid: false,
-        error: {
-                Digit: 'password must have at least one digit',
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-
-    const result = validate('ocean');
-    expect(result).toEqual(expected);
-})
-
-it('tests if acclimatization is invalid and matches errors', () =>{
-    const expected = {
-        isValid: false,
-        error: {
-                Digit: 'password must have at least one digit',
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-
-    const result = validate('acclimatization');
-    expect(result).toEqual(expected);
-})
-
-// tests fails for uppercase only
-  it('tests if ocean11 is invalid', () =>{
-    const expected = {
-        isValid: false,
-        error: {
-                Case: 'password must have at least one Uppercase'
-        }
-    }
-
-    const result = validate('ocean11');
-    expect(result).toEqual(expected);
-  })
-
-
-// testing fails for length only
-it('tests if Cat1 is invalid and matches errors', () =>{
-    const expected = {
-        isValid: false,
-        error: {
-            Length: 'password must be between 5 and 15 characters', 
-        }
-    }
-
-    const result = validate('Cat1');
-    expect(result).toEqual(expected);
-})
-
-it('tests if Electroencephalograph7 is invalid and matches errors', () =>{
-    const expected = {
-        isValid: false,
-        error: {
-            Length: 'password must be between 5 and 15 characters', 
-        }
-    }
-
-    const result = validate('Electroencephalograph7');
-    expect(result).toEqual(expected);
-})
-
-// tests fails for digit only
-
-it('tests if Ocean is valid', () =>{
-    const expected = {
-        isValid: false,
-        error: {
-            Digit: 'password must have at least one digit', 
-        }
-    }
-
-    const result = validate('Ocean');
-    expect(result).toEqual(expected);
-})
-
-  // all errors are avoided 
-
-  it('tests if Ocean11 is valid', () =>{
-    const expected = {
-        isValid: true,
-        error: {
+            Case: 'password must have at least one Uppercase'
+            }
+        }],
+        
+        ['electroencephalograph3', {
+            isValid: false,
+            error: {
+                    Length: 'password must be between 5 and 15 characters',
+                    Case: 'password must have at least one Uppercase'
+            }
             
+        }],
+        ['ocean', {
+            isValid: false,
+            error: {
+                    Digit: 'password must have at least one digit',
+                    Case: 'password must have at least one Uppercase'
+            }
+        }],
+        ['acclimatization', {
+            isValid: false,
+            error: {
+                    Digit: 'password must have at least one digit',
+                    Case: 'password must have at least one Uppercase'
+            }
+        }],
+        ['ocean11', {
+            isValid: false,
+            error: {
+                    Case: 'password must have at least one Uppercase'
+            }
+        }],
+        ['Cat1', {
+            isValid: false,
+            error: {
+            Length: 'password must be between 5 and 15 characters', 
         }
-    }
+        }],
+        ['Electroencephalograph7', {
+            isValid: false,
+            error: {
+                Length: 'password must be between 5 and 15 characters', 
+            }
+        }],
+        ['Ocean', {
+            isValid: false,
+            error: {
+                Digit: 'password must have at least one digit', 
+            }
+        }],
+        ['Ocean11', {
+            isValid: true,
+            error: {}
+        }],
+        ['', {
+            isValid: false,
+            error: {
+            Length: 'password must be between 5 and 15 characters',
+            Digit: 'password must have at least one digit',
+            Case: 'password must have at least one Uppercase'
+        }
+        }],
 
-    const result = validate('Ocean11');
-    expect(result).toEqual(expected);
-})
 
-})
+    ]
+
+ testCases.forEach(([input, expected]) => {
+    let errorCount =  Object.keys(expected.error).length
+    let errorKind = Object.keys(expected.error).join(', ')
+    it(`tests if "${input}" yields ${errorCount} errors: ${errorKind}`, () => {
+      const result = validate(input);
+      expect(result).toEqual(expected);
+    });
+  });
+    
+});
 
 
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,18 +1,19 @@
 
 
+
+
 export default function validate(input: string) {
 
     const message = {
         isValid: true,
         error: {} as Record<string, string>
-        // error: null as null | string // type assertion in typescript, error can be either null or string
     }
 
     if (input.length < 5 || input.length > 15) {
         message.isValid = false
         message.error.Length = "password must be between 5 and 15 characters"
     }
-    
+
     if (!/[0-9]/.test(input)) {
         message.isValid = false
         message.error.Digit = 'password must have at least one digit'
@@ -23,9 +24,6 @@ export default function validate(input: string) {
         message.error.Case = 'password must have at least one Uppercase'
     }
 
-    
-
-    console.log(message);
     return message;
 
 }
