567e4316872495e1e211a54286bc6cea3b829d8c
quirkexplorer <quirkyexplorer@gmail.com>
2023-10-28 11:32:35 -0700

    comitting on green, no implementation

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import validateRange from './index'
 
-describe('military time validator', () => {
+describe('military time validator passing tests', () => {
 
-
+    it('knows 05:05 - 05:06 is a valid range', () => {
+        expect(validateRange('05:05 - 05:06')).toBe(true);
+    })
 })
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,6 @@
+export default function validateRange(timeRange: string): boolean{
+    const valid = true;
+    const invalid = false;
+
+    return valid;
+}
