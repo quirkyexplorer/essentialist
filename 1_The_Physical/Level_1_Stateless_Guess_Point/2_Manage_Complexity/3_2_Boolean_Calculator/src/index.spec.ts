1879fa6a8555d4d566dedfdf02a8998cadd2b381
quirkexplorer <quirkyexplorer@gmail.com>
2023-11-04 12:06:47 -0700

    ' on green, simple fake it implementation'

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,15 @@
+import {EvaluateBoolean} from "./index"
 
 describe('boolean calculator', () => {
+  it('knows TRUE to be true',() =>{
 
+    const  result = EvaluateBoolean("TRUE");
+    expect(result).toBe(true);
+  })
+
+  it('knows FALSE to be false',() =>{
+
+    const  result = EvaluateBoolean("FALSE");
+    expect(result).toBe(false);
+  })
 })
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,11 @@
+
+export function EvaluateBoolean(value: string) {
+
+    if( value == "TRUE") 
+        return true
+
+    else if (value == "FALSE")
+        return false
+
+
+}
